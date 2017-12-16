29f7e61a8788c50302c0a17a81b187ee1d378d06
lederdavid <lederkremer@gmail.com>
2017-12-16 19:48:43 +0200

    ignoring albums for now because there seems to be a problem

--- miner.py
+++ miner.py
@@ -1,5 +1,5 @@
-import pylast
 import MySQLdb as mdb
+import pylast
 
 API_KEY = "d202d3c8b0726f003b32954d7d37e6ab"
 API_SECRET = "186006795c45cabe0d4692cd8dd6b01c"
@@ -42,8 +42,8 @@ def insert_new_album(con, album_name, artist_id, album_counter):
 def insert_new_song(con, title, artist_id, album_id, song_counter):
     with con:
         cur = con.cursor()
-        cur.execute("SELECT song_id FROM songs WHERE name = %s AND artist_id = %s AND album_id = %s",
-                    (title, artist_id, album_id))
+        cur.execute("SELECT song_id FROM songs WHERE name = %s AND artist_id = %s",
+                    (title, artist_id))
         if cur.rowcount == 0:
             cur.execute("INSERT INTO songs (song_id, name, album_id, artist_id) VALUES(%s, %s, %s, %s)",
                         (song_counter, title, album_id, artist_id))
@@ -57,19 +57,23 @@ def get_tracks():
     network = pylast.LastFMNetwork(api_key=API_KEY, api_secret=API_SECRET,
                                    username=username, password_hash=password_hash)
     con = mdb.connect('localhost', 'root', 'Armageddon1', "mr_music")
-    chart_songs = network.get_top_tracks(20000)
+    chart_songs = network.get_top_tracks(1000)
     artist_counter = 0
     album_counter = 0
     song_counter = 0
     for song in chart_songs:
         artist, title = get_title_and_artist_from_item(str(song.item))
-        track = network.get_track(artist, title)
-        artist_counter = insert_new_artist(con, artist, artist_counter)
-        if track is not None and track.get_album() is not None:
-            album_counter = insert_new_album(con, track.get_album().get_name(), artist_counter, album_counter)
-        else:
-            album_counter = insert_new_album(con, "unknown", artist_counter, album_counter)
-        song_counter = insert_new_song(con, title, artist_counter, album_counter, song_counter)
+        artist_id = insert_new_artist(con, artist, artist_counter)
+        # try:
+        #     track = network.get_track(artist, title)
+        #     if track is not None and track.get_album() is not None:
+        #         album_id = insert_new_album(con, track.get_album().get_name(), artist_id, album_counter)
+        #     else:
+        #         album_id = insert_new_album(con, "unknown", artist_id, album_counter)
+        #     song_counter = insert_new_song(con, title, artist_id, album_id, song_counter)
+        # except pylast.NetworkError:
+        #     song_counter = insert_new_song(con, title, artist_id, -1, song_counter)
+        insert_new_song(con, title, artist_id, -1, song_counter)
         artist_counter += 1
         album_counter += 1
         song_counter += 1

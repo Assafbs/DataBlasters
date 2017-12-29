322a11999a4e0595050db3b3587d04bfd5165d2d
lederdavid <lederkremer@gmail.com>
2017-12-29 11:27:08 +0200

    word scraper for Tal. You're welcome.

---
+++ Common/word_scraper.py
@@ -0,0 +1,67 @@
+import operator
+import string
+import re
+
+
+def has_numbers(input_string):
+    return any(char.isdigit() for char in input_string)
+
+
+def has_letters(word):
+    if not re.search('[a-zA-Z]', word):
+        print word
+
+
+def has_only_letters(word):
+    for ch in word:
+        if ch not in string.letters:
+            return False
+    return True
+
+
+def replace_punctuations(word):
+    exclude = set(string.punctuation)
+    return ''.join(ch for ch in word if ch not in exclude)
+
+
+def replace_slashn(word):
+    # exclude = ['\n','\t','\r', '\u']
+    exclude = string.whitespace
+    return ''.join(ch for ch in word if ch not in exclude)
+
+
+def get_dict_word_count(text):
+    dict_of_words = dict()
+    for word in text.split(' '):
+        word = word.lower()
+        word = replace_punctuations(word)
+        word = replace_slashn(word)
+
+        if has_only_letters(word):
+            if word not in dict_of_words:
+                dict_of_words.update({word: 1})
+            else:
+                dict_of_words.update({word: dict_of_words[word] + 1})
+    return dict_of_words
+
+
+def print_dict(dictionary):
+    for key in dictionary.keys():
+        if len(dictionary[key]) > 3:
+            print "key: " + key
+            for word in dictionary[key]:
+                print word
+            print "\n"
+
+
+def sort_dict_by_value(dict_of_words):
+    sorted_list = sorted(dict_of_words.items(), key=operator.itemgetter(1))
+    return sorted_list
+
+
+if __name__ == '__main__':
+    text = "Hello, I love you, won't you tell me your name? you you name"
+    my_dict = get_dict_word_count(text)
+    lst = sort_dict_by_value(my_dict)
+    lst.reverse()
+    print lst

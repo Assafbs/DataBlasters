68c9c2be23776be5d2e3219095759c9b020c6ee8
Assaf Ben Shimon <asafbs@gmail.com>
2018-01-08 00:06:40 +0200

    Modified DbConnector
    - Removed get_connection as it's not needed
    - Added methods for executing queries with arguments

--- db_connector.py
+++ db_connector.py
@@ -13,10 +13,6 @@ class DbConnector:
         pass
 
     @staticmethod
-    def get_connection():
-        return mdb.connect(ADDRESS, USERNAME, PASSWORD, SCHEMA)
-
-    @staticmethod
     def get_result_for_query(query):
         con = mdb.connect(ADDRESS, USERNAME, PASSWORD, SCHEMA)
         with con:
@@ -25,11 +21,27 @@ class DbConnector:
             return cur.fetchone()
 
     @staticmethod
+    def get_result_for_query_with_args(query, arguments):
+        con = mdb.connect(ADDRESS, USERNAME, PASSWORD, SCHEMA)
+        with con:
+            cur = con.cursor()
+            cur.execute(query, arguments)
+            return cur.fetchone()
+
+    @staticmethod
     def get_all_results_for_query(query):
         con = mdb.connect(ADDRESS, USERNAME, PASSWORD, SCHEMA)
         with con:
             cur = con.cursor()
             cur.execute(query)
+            return cur.fetchall()
+
+    @staticmethod
+    def get_all_results_for_query_with_args(query, arguments):
+        con = mdb.connect(ADDRESS, USERNAME, PASSWORD, SCHEMA)
+        with con:
+            cur = con.cursor()
+            cur.execute(query, arguments)
             return cur.fetchall()
 
     @staticmethod

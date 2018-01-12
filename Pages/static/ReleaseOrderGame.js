7b44da46fe31aabdc43a9426335235da379725ed
Tal Solomon <talsolomon3@gmail.com>
2018-01-12 16:51:47 +0200

    release order song - add answer validation

--- Pages/static/ReleaseOrderGame.js
+++ Pages/static/ReleaseOrderGame.js
@@ -2,3 +2,14 @@ function Redirect(path) {
   document.cookie = 'allowAccess=true';
   window.location.href = path;
 }
+
+function validateForm() {
+    var song1 = document.forms["answers"]["song1"].value;
+    var song2 = document.forms["answers"]["song2"].value;
+    var song3 = document.forms["answers"]["song3"].value;
+    var song4 = document.forms["answers"]["song4"].value;
+    if ((song1 == song2) || (song1 == song3) ||(song1 == song4) ||(song21 == song3) ||(song2 == song4) ||(song3 == song4)) {
+        alert('You cant choose the same song twice!!');
+        return false;
+    }
+}

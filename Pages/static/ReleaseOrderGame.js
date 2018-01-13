a88e94abd7b45521f9c83212dea3b7ed299dab5c
Tal Solomon <talsolomon3@gmail.com>
2018-01-13 09:42:32 +0200

    release order game - bug fix

--- Pages/static/ReleaseOrderGame.js
+++ Pages/static/ReleaseOrderGame.js
@@ -8,7 +8,7 @@ function validateForm() {
     var song2 = document.forms["answers"]["song2"].value;
     var song3 = document.forms["answers"]["song3"].value;
     var song4 = document.forms["answers"]["song4"].value;
-    if ((song1 == song2) || (song1 == song3) ||(song1 == song4) ||(song21 == song3) ||(song2 == song4) ||(song3 == song4)) {
+    if ((song1 == song2) || (song1 == song3) ||(song1 == song4) ||(song2 == song3) ||(song2 == song4) ||(song3 == song4)) {
         alert('You cant choose the same song twice!!');
         return false;
     }

21a43bed23f986d15cf014009bbc9480e7e5dd4e
Tal Solomon <talsolomon3@gmail.com>
2018-01-13 12:06:19 +0200

    beutify translation game queries

--- query_generator.py
+++ query_generator.py
@@ -12,42 +12,42 @@ class QueryGenerator:
 
     @staticmethod
     def get_translated_song_question_query():
-        return "SELECT lyrics.song_id, lyrics.lyrics, lyrics.lyrics_language, lyrics.hebrew_translation, songs.title\n" \
-               "FROM lyrics JOIN songs ON lyrics.song_id = songs.song_id\n" \
-               "WHERE lyrics.hebrew_translation IS NOT NULL\n" \
-               "ORDER BY rand()\n" \
-               "LIMIT 1"
+        return """SELECT lyrics.song_id, lyrics.lyrics, lyrics.lyrics_language, lyrics.hebrew_translation, songs.title\n
+               FROM lyrics JOIN songs ON lyrics.song_id = songs.song_id\n
+               WHERE lyrics.hebrew_translation IS NOT NULL\n
+               ORDER BY rand()\n
+               LIMIT 1"""
 
     @staticmethod
     def get_translated_song_answers_query():
-        return "SELECT DISTINCT songs.title, (count(*) - 1) AS numWords\n" \
-               "FROM songs JOIN (\n" \
-               "(SELECT song_id \n" \
-               "FROM lyrics\n" \
-               "WHERE MATCH(lyrics) AGAINST(%s IN BOOLEAN MODE) AND lyrics_language = %s)\n" \
-               "UNION ALL\n" \
-               "(SELECT song_id \n" \
-               "FROM lyrics \n" \
-               "WHERE MATCH(lyrics) AGAINST(%s IN BOOLEAN MODE) AND lyrics_language = %s)\n" \
-               "UNION ALL\n" \
-               "(SELECT song_id \n" \
-               "FROM lyrics \n" \
-               "WHERE MATCH(lyrics) AGAINST(%s IN BOOLEAN MODE) AND lyrics_language = %s)\n" \
-               "UNION ALL\n" \
-               "(SELECT song_id \n" \
-               "FROM lyrics\n" \
-               "WHERE MATCH(lyrics) AGAINST(%s IN BOOLEAN MODE) AND lyrics_language = %s)\n" \
-               "UNION ALL\n" \
-               "(SELECT song_id \n" \
-               "FROM lyrics\n" \
-               "WHERE MATCH(lyrics) AGAINST(%s IN BOOLEAN MODE) AND lyrics_language = %s)\n" \
-               "UNION ALL \n" \
-               "(SELECT song_id FROM lyrics)\n" \
-               ") AS wordsCnt ON wordsCnt.song_id = songs.song_id\n" \
-               "WHERE wordsCnt.song_id <> %s AND songs.title <> %s\n" \
-               "GROUP BY wordsCnt.song_id\n" \
-               "ORDER BY numWords DESC\n" \
-               "LIMIT 3"
+        return """SELECT DISTINCT songs.title, (count(*) - 1) AS numWords\n
+               FROM songs JOIN (\n
+                   (SELECT song_id \n
+                   FROM lyrics\n
+                   WHERE MATCH(lyrics) AGAINST(%s IN BOOLEAN MODE) AND lyrics_language = %s)\n
+                   UNION ALL\n
+                   (SELECT song_id \n
+                   FROM lyrics \n
+                   WHERE MATCH(lyrics) AGAINST(%s IN BOOLEAN MODE) AND lyrics_language = %s)\n
+                   UNION ALL\n
+                   (SELECT song_id \n
+                   FROM lyrics \n
+                   WHERE MATCH(lyrics) AGAINST(%s IN BOOLEAN MODE) AND lyrics_language = %s)\n
+                   UNION ALL\n
+                   (SELECT song_id \n
+                   FROM lyrics\n
+                   WHERE MATCH(lyrics) AGAINST(%s IN BOOLEAN MODE) AND lyrics_language = %s)\n
+                   UNION ALL\n
+                   (SELECT song_id \n
+                   FROM lyrics\n
+                   WHERE MATCH(lyrics) AGAINST(%s IN BOOLEAN MODE) AND lyrics_language = %s)\n
+                   UNION ALL \n
+                   (SELECT song_id FROM lyrics)\n
+                   ) AS wordsCnt ON wordsCnt.song_id = songs.song_id\n
+               WHERE wordsCnt.song_id <> %s AND songs.title <> %s\n
+               GROUP BY wordsCnt.song_id\n
+               ORDER BY numWords DESC\n
+               LIMIT 3"""
 
     @staticmethod
     def get_top_ten_query():

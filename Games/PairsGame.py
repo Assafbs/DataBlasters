6d97591200880aa6a6e14b8ef0f27518098c3da3
lederdavid <lederkremer@gmail.com>
2018-01-13 19:39:33 +0200

    added album covers by artists questions

--- Games/PairsGame.py
+++ Games/PairsGame.py
@@ -8,14 +8,15 @@ from query_generator import QueryGenerator
 app = Flask(__name__)  # TODO: delete this, it should be only in 1 place (main page or something). this is just for debugging
 connector = DbConnector()
 NUM_QUESTIONS_PER_GAME = 5
-game_manager = GameManager.GameManager()
+GAME_ID = 3
+game_manager = GameManager.GameManager(GAME_ID)
 pairs_game = Blueprint('pairs_game', __name__, template_folder='templates')
 pairs_game_ = Blueprint('pairs_game_', __name__, template_folder='templates')
 
 
 @pairs_game.route('/pairs_game')
 def pairs_game_start():
-    game_manager.start_new_game(3)
+    game_manager.start_new_game()
     return create_game_page()
 
 
@@ -31,9 +32,9 @@ def pairs_game_mid():
         return response
 
 
-def get_list_of_artists(n):
+def get_list_of_results(query, n):
     lst_of_artists = list()
-    rows = connector.get_all_results_for_query(QueryGenerator.get_n_random_artists(), n)
+    rows = connector.get_all_results_for_query(query, n)
     for row in rows:
         lst_of_artists.append((row[0]))
     return lst_of_artists
@@ -51,7 +52,7 @@ def get_n_random_songs_from_artist(n, artist_id):
 
 def get_all_songs():
     # lst_of_artists = DbConnector.get_n_random_artists(7)
-    lst_of_artists = get_list_of_artists(7)
+    lst_of_artists = get_list_of_results(QueryGenerator.get_n_random_artists(), 7)
     winner_artist = random.randint(0, 6)
     lst_of_bad_songs = list()
     winner_songs = list()
@@ -70,7 +71,7 @@ def translate_artist_id_list_to_artist_name_list(ids):
     return lst_of_names
 
 
-def get_winning_artists():
+def get_winning_artists_from_countries():
     lst_of_artists = list()
     country = connector.get_all_results_for_query(QueryGenerator.get_n_random_countries(), 1)[0][0]
     rows = connector.get_all_results_for_query(QueryGenerator.get_n_artists_from_country(), (country, 2))
@@ -80,7 +81,7 @@ def get_winning_artists():
     return lst_of_artists
 
 
-def get_bad_artists():
+def get_bad_artists_from_countries():
     lst_of_artists = list()
     for i in range(3):
         countries = connector.get_all_results_for_query(QueryGenerator.get_n_random_countries(), 2)
@@ -90,15 +91,66 @@ def get_bad_artists():
     return lst_of_artists
 
 
+def get_n_random_covers_from_artist(n, artist_id):
+    lst_of_covers = list()
+    rows = connector.get_all_results_for_query(QueryGenerator.get_n_album_covers_from_artist(), (artist_id, n))
+    for row in rows:
+        lst_of_covers.append(row[0])
+    return lst_of_covers
+
+
+def get_all_covers():
+    lst_of_artists = get_list_of_results(QueryGenerator.get_n_random_artists_from_possible_artists(), 7)
+    winner_artist = random.randint(0, 6)
+    bad_covers = list()
+    winner_covers = list()
+    for i in range(7):
+        if i == winner_artist:
+            winner_covers = get_n_random_covers_from_artist(2, lst_of_artists[i])
+        else:
+            bad_covers.append(get_n_random_covers_from_artist(1, lst_of_artists[i]))
+    return winner_covers, bad_covers
+
+
 def create_game_page():
+    reload(sys)
+    sys.setdefaultencoding('UTF8')
+
+    connector.execute_query(QueryGenerator.create_view_possible_artists())
+    winner_covers, bad_covers = get_all_covers()
+    right_answer = winner_covers[0] + "!@" + winner_covers[1]
+    wrong_answers = calc_answers_cover_pairs(bad_covers)
+    answers = random.sample(wrong_answers + [right_answer], 4)
+    try:
+        response = make_response(render_template('PairsGameCovers.html',
+                                                 question=" Which of these pairs of album covers are by the same artist?",
+                                                 option_1_1=answers[0].split('!@')[0],
+                                                 option_1_2=answers[0].split('!@')[1],
+                                                 option_2_1=answers[1].split('!@')[0],
+                                                 option_2_2=answers[1].split('!@')[1],
+                                                 option_3_1=answers[2].split('!@')[0],
+                                                 option_3_2=answers[2].split('!@')[1],
+                                                 option_4_1=answers[3].split('!@')[0],
+                                                 option_4_2=answers[3].split('!@')[1],
+                                                 current_score=game_manager.score))
+        response.set_cookie('correctAnswerNum', str(answers.index(right_answer) + 1))
+        return game_manager.update_cookies_for_new_question(response)
+    except Exception as e:
+        print "Error occurred with response"
+        print e.message
+        create_game_page()
+    return
+
+
+def create_game_page3():
     reload(sys)
     sys.setdefaultencoding('UTF8')
     # DbConnector.create_view_songs_per_artists()  # need to drop view when finish game
     connector.execute_query(QueryGenerator.create_view_artists_per_country())
-    winner_artists = translate_artist_id_list_to_artist_name_list(get_winning_artists())
-    lst_of_bad_artists = translate_artist_id_list_to_artist_name_list(get_bad_artists())
+    winner_artists = translate_artist_id_list_to_artist_name_list(get_winning_artists_from_countries())
+    lst_of_bad_artists = translate_artist_id_list_to_artist_name_list(get_bad_artists_from_countries())
     right_answer = winner_artists[0] + "!@" + winner_artists[1]
-    wrong_answers = calc_answers_artists_pairs(lst_of_bad_artists)
+    wrong_answers = calc_answers_artist_pairs(lst_of_bad_artists)
     answers = random.sample(wrong_answers + [right_answer], 4)
     try:
         response = make_response(render_template('PairsGame.html',
@@ -159,10 +211,17 @@ def calc_answers_song_pairs(bad_songs):
     return lst_of_answers
 
 
-def calc_answers_artists_pairs(bad_artists):
+def calc_answers_artist_pairs(bad_artists):
     lst_of_answers = list()
     for i in range(3):
         lst_of_answers.append(bad_artists.pop() + "!@" + bad_artists.pop())
+    return lst_of_answers
+
+
+def calc_answers_cover_pairs(bad_covers):
+    lst_of_answers = list()
+    for i in range(3):
+        lst_of_answers.append(bad_covers.pop()[0] + "!@" + bad_covers.pop()[0])
     return lst_of_answers
 
 

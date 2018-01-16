b239362332cae094a23970d2c1b6970b3466541d
Tal Solomon <talsolomon3@gmail.com>
2018-01-16 15:44:56 +0200

    show nickname and score from cookie + enforce sign in

--- Pages/GameSelection.py
+++ Pages/GameSelection.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, make_response, Blueprint, request, redirect
+import Common.common
 from query_generator import QueryGenerator
 
 game_selection = Blueprint('game_selection', __name__, template_folder='templates')
@@ -6,10 +7,9 @@ game_selection = Blueprint('game_selection', __name__, template_folder='template
 
 @game_selection.route('/game_selection')
 def create_game_selection_page():
-    # TODO: replace with real score
-    cookie_state=request.cookies.get('nickname')
-    if(cookie_state is None):
-        return redirect("http://127.0.0.1:5000/sign_up")
-    score=request.cookies.get('score')
-    response = make_response(render_template('game_selection.html', current_score=score))
+    nickname = Common.common.get_value_from_cookie(request, 'nickname')
+    if nickname is None:
+        return redirect('/')
+    score = Common.common.get_value_from_cookie(request, 'score')
+    response = make_response(render_template('game_selection.html', score=score, nickname=nickname))
     return response

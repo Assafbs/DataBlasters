eb9342231c6a56222e71c235433c7bf1068d5b5d
michalwass <wasserlauf@mail.tau.ac.il>
2018-01-19 12:15:43 +0200

    update logout and word_scrapper

--- Common/word_scraper.py
+++ Common/word_scraper.py
@@ -34,7 +34,7 @@ def get_dict_word_count(text):
     wordsToIgnore = ['a', 'although', 'an', 'are', 'as', 'because', 'before', 'el', 'for', 'her', 'i', 'if',
                      'in', 'is', 'it', 'la', 'me', 'mi', 'my', 'on', 'once', 'only', 'of', 'so', 'te', 'the',
                      'these', 'than', 'that', 'though', 'till', 'to', 'unless', 'when', 'whenever', 'where',
-                     'wherever', 'while', 'with', 'you', 'use', 'lyrics', 'commercial', '', ' ']
+                     'wherever', 'while', 'with', 'you', 'was', '', ' ','were','be','he','she','im','your','and',]
     # TODO can remove use,lyrics,commercial from list when working with clean lyrics
     dict_of_words = dict()
     for word in text.split():  # changd from split(' ') to split()

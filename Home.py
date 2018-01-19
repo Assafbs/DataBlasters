cd6c52f8136efc530a1ae390126d5e33868f67d0
michalwass <wasserlauf@mail.tau.ac.il>
2018-01-19 14:35:39 +0200

    new login,signup,new_pass templates

--- Home.py
+++ Home.py
@@ -60,5 +60,5 @@ def page_not_found(e):
 
 
 if __name__ == '__main__':
-    app.run(port=40557, host='0.0.0.0')
-    # app.run(debug=True)
+    #app.run(port=40557, host='0.0.0.0')
+    app.run(debug=True)

d24a05cabbea23d113a5f7a0a755592747fcb4ed
lederdavid <lederkremer@gmail.com>
2018-01-19 18:08:20 +0200

    fixed jumbotron bug

--- Common/db_connector.py
+++ Common/db_connector.py
@@ -1,22 +1,23 @@
 import MySQLdb as mdb
 
-# ADDRESS = 'mysqlsrv.cs.tau.ac.il'  # '127.0.0.1'
-ADDRESS = 'mysqlsrv.cs.tau.ac.il'  # 'localhost'
-USERNAME = 'DbMysql09'  # 'root'
-PASSWORD = 'DbMysql09'  # 'root'
-SCHEMA = 'DbMysql09'  # 'dbmysql09'
+ADDRESS = 'mysqlsrv.cs.tau.ac.il'
+USERNAME = 'DbMysql09'
+PASSWORD = 'DbMysql09'
+SCHEMA = 'DbMysql09'
 
 
 class DbConnector:
 
     def __init__(self):
-        self.con = mdb.connect(ADDRESS, USERNAME, PASSWORD, SCHEMA, port=3306)
+        self.con = mdb.connect(ADDRESS, USERNAME, PASSWORD, SCHEMA, port=3306)  # connect to Nova DB
         self.cur = self.con.cursor()
 
+    # commit and close connection
     def close(self):
         self.con.commit()
         self.con.close()
 
+    # execute query with optional arguments. This is used for insert/update statements
     def execute_query(self, query, arguments=None):
         if arguments is None:
             self.cur.execute(query)
@@ -24,6 +25,7 @@ class DbConnector:
             self.cur.execute(query, arguments)
         self.con.commit()
 
+    #  execute query with optional arguments, return first result
     def get_one_result_for_query(self, query, arguments=None):
         if arguments is None:
             self.cur.execute(query)
@@ -31,6 +33,7 @@ class DbConnector:
             self.cur.execute(query, arguments)
         return self.cur.fetchone()
 
+    # execute query with optional arguments, return all results
     def get_all_results_for_query(self, query, arguments=None):
         if arguments is None:
             self.cur.execute(query)
